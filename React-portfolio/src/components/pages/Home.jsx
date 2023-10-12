ed99dc6d245e7a7cd536508d1c75a7a548df97f4
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-12 19:05:44 -0400

    added home function

---
+++ React-portfolio/src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Alexandra Zykova</h1>
+        <p>
+          Welcome to My React Portfolio
+        </p>
+      </div>
+    );
+  }
