c6c704147ba7a340a036d062bd257243ccb76b81
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-13 16:59:34 -0400

    testing img

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,7 +5,7 @@ export default function Portfolio() {
         <p>
             {/* titled images of 6 links projects to both the deployed and GitHub repo */}
           Work Samples
-          <img src='./public/vite.svg'></img>
+          <img src='https://user-images.githubusercontent.com/140107498/275048879-d4375139-5fd1-4f12-9e88-fa3bedb71cd1.png'></img>
           <img src='./public/ex_logo.svg.png'></img>
         </p>
       </div>
