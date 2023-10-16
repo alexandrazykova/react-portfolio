2944a01ea3d5991e13f903a1b6c5215324a1014b
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-15 22:42:25 -0400

    added more project items to portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -18,6 +18,27 @@ export default function Portfolio() {
                 </a>
             </div>
             <div className="work-item col-md-3 col-10">
+                <a href="https://drive.google.com/file/d/14EKRkIHwyIkg5-IRo1YA9XGnyefsmqOV/view?usp=sharing">
+                    <img src="https://user-images.githubusercontent.com/140107498/275375290-06343ea9-22ed-4ffd-a293-d416860bafe4.png" alt="E-commerce"></img>
+                    <p className="project-name">E-commerce
+                    <a href="https://github.com/alexandrazykova/e-commerce"><br></br> Github Repo</a></p>
+                    </a>
+            </div>
+            <div className="work-item col-md-3 col-10">
+                <a href="https://drive.google.com/file/d/1YYqsqCM05_YQL0Z_ZLbaEGjNjcvW0gcb/view?usp=sharing">
+                    <img src="https://github.com/alexandrazykova/SVG-Logo-Maker/blob/main/examples/ex_logo.svg.png?raw=true" alt="SVG logo generator"></img>
+                    <p className="project-name">SVG Logo Generator
+                    <a href="https://github.com/alexandrazykova/SVG-Logo-Maker"><br></br> Github Repo</a></p>
+                </a>
+            </div>
+            <div className="work-item col-md-3 col-10">
+                <a href="https://drive.google.com/file/d/1XyecRhReQ508MSSJk8ZNAZuoU5AKe8KI/view?usp=sharing">
+                    <img src="https://user-images.githubusercontent.com/140107498/275376997-36c24e9a-427e-4e12-99ed-b1912c8d4e1f.png" alt="Employee Tracker"></img>
+                    <p className="project-name">Employee Tracker
+                    <a href="https://github.com/alexandrazykova/employee-tacker"><br></br> Github Repo</a></p>
+                    </a>
+            </div>
+            <div className="work-item col-md-3 col-10">
                 <a href="https://alexandrazykova.github.io/password-generator">
                     <img src="https://raw.githubusercontent.com/alexandrazykova/professional-portfolio/main/images/PG2.png" alt="Password Generator Project"></img>
                     <p className="project-name">Password Generator
@@ -29,6 +50,13 @@ export default function Portfolio() {
                     <img src="https://raw.githubusercontent.com/alexandrazykova/professional-portfolio/main/images/scheduler-screenshot%20copy.png" alt="Work Day Scheduler"></img>
                     <p className="project-name">Work Day Scheduler
                     <a href="https://github.com/alexandrazykova/Work-Day-Scheduler"><br></br> Github Repo</a></p>
+                </a>
+            </div>
+            <div className="work-item col-md-3 col-10">
+                <a href="https://drive.google.com/file/d/1NxLOvXU5EdgJK-kkFMFJ-VWMQ0hCW0p5/view?usp=sharing">
+                    <img src="https://user-images.githubusercontent.com/140107498/275374910-169f9f7c-7f81-4fe1-b66a-99f8790a0789.png" alt="Readme generator"></img>
+                    <p className="project-name">Readme.md Generator
+                    <a href="https://github.com/alexandrazykova/readme-generator"><br></br> Github Repo</a></p>
                 </a>
             </div>
             <div className="work-item col-md-3 col-10">
@@ -65,6 +93,13 @@ export default function Portfolio() {
                     <p className="project-name">Portfolio 1.0
                     <a href="https://github.com/alexandrazykova/professional-portfolio"><br></br> Github Repo</a></p>
                 </a>
+            </div>
+            <div className="work-item col-md-3 col-10">
+                <a href="https://drive.google.com/file/d/13d5ZPf8lXgGKQFO3bqDhjEEKpJp-uidx/view?usp=sharing">
+                    <img src="https://user-images.githubusercontent.com/140107498/275375896-b9205234-61f3-4439-853e-740961a96752.png" alt="Social Network API"></img>
+                    <p className="project-name">Social Network API
+                    <a href="https://github.com/alexandrazykova/social-network-api"><br></br> Github Repo</a></p>
+                    </a>
             </div>
         </div>
     </section>
